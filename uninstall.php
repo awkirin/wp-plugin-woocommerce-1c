35a5ac0c3bf288760d36a93193af95331738b718
AWKirin <awkirin@yandex.ru>
2025-02-17 18:12:45 +0300

    fix

--- uninstall.php
+++ uninstall.php
@@ -3,8 +3,8 @@ if (!defined('WP_UNINSTALL_PLUGIN') && !defined('WP_CLI')) exit;
 
 if (!defined('WC1C_PLUGIN_DIR')) define('WC1C_PLUGIN_DIR', __DIR__ . '/');
 if (!defined('WC1C_DATA_DIR')) {
-  $upload_dir = wp_upload_dir();
-  define('WC1C_DATA_DIR', "{$upload_dir['basedir']}/woocommerce-1c/");
+	$upload_dir = wp_upload_dir();
+	define('WC1C_DATA_DIR', "{$upload_dir['basedir']}/woocommerce-1c/");
 }
 
 require WC1C_PLUGIN_DIR . "exchange.php";
@@ -13,11 +13,11 @@ wc1c_disable_time_limit();
 global $wpdb;
 
 if (is_dir(WC1C_DATA_DIR)) {
-  $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(WC1C_DATA_DIR, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST);
-  foreach ($iterator as $path => $item) {
-    $item->isDir() ? rmdir($path) : unlink($path);
-  }
-  rmdir(WC1C_DATA_DIR);
+	$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(WC1C_DATA_DIR, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST);
+	foreach ($iterator as $path => $item) {
+		$item->isDir() ? rmdir($path) : unlink($path);
+	}
+	rmdir(WC1C_DATA_DIR);
 }
 
 // $term_meta_keys = $wpdb->get_col("SELECT DISTINCT meta_key FROM $wpdb->termmeta WHERE meta_key LIKE 'wc1c_%'");
@@ -38,14 +38,14 @@ if (is_dir(WC1C_DATA_DIR)) {
 // }
 
 $index_table_names = array(
-  $wpdb->postmeta,
-  $wpdb->termmeta,
-  $wpdb->usermeta,
+	$wpdb->postmeta,
+	$wpdb->termmeta,
+	$wpdb->usermeta,
 );
 foreach ($index_table_names as $index_table_name) {
-  $index_name = 'wc1c_meta_key_meta_value';
-  $result = $wpdb->get_var("SHOW INDEX FROM $index_table_name WHERE Key_name = '$index_name';");
-  if (!$result) continue;
+	$index_name = 'wc1c_meta_key_meta_value';
+	$result = $wpdb->get_var("SHOW INDEX FROM $index_table_name WHERE Key_name = '$index_name';");
+	if (!$result) continue;
 
-  $wpdb->query("DROP INDEX $index_name ON $index_table_name");
+	$wpdb->query("DROP INDEX $index_name ON $index_table_name");
 }
